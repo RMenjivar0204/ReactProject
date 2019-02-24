b4cbe8faddc0baa81e9ae7d1f4fde6a9913b4d77
RMenjivar0204 <raul.menjivar4@gmail.com>
2019-02-24 14:21:02 -0600

    color

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col, ButtonToolbar, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
+
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Container>
+          <Row className="show-grid" >
+            <Col xs={12} md={8} >
+              <div style={styles.starStyles}>this is my first column</div>
+
+              <ButtonToolbar>
+                <Button variant="outline-primary">Primary</Button>
+                <Button variant="outline-secondary">Secondary</Button>
+                <Button variant="outline-success">Success</Button>
+                <Button variant="outline-warning">Warning</Button>
+                <Button variant="outline-danger">Danger</Button>
+                <Button variant="outline-info">Info</Button>
+                <Button variant="outline-light">Light</Button>
+                <Button variant="outline-dark">Dark</Button>
+              </ButtonToolbar>
+            </Col>
+            <Col xs={12} md={4}>
+            this is my second column
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
+  }
+}
+
+const styles = {
+  starStyles: {
+    color: "Orange",
+    backgroundColor: "Green"
   }
 }
 
